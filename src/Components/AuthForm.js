7e2fd5e977cfdf84dcfdd4b040e4920e5666ee59
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-01-10 22:46:05 -0700

    Error message

--- src/Components/AuthForm.js
+++ src/Components/AuthForm.js
@@ -10,17 +10,19 @@ export default function AuthForm({
 }) {
   return (
     <div>
-      <h2>{errorMessage}</h2>
-      <label>Email: </label>
-      <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <label>Password: </label>
-      <input
-        type="text"
-        name="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Save</button>
+      <form>
+        <h2>{errorMessage}</h2>
+        <label>Email: </label>
+        <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <label>Password: </label>
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Save</button>
+      </form>
     </div>
   );
 }
